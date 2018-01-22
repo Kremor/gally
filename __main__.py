ab2466ebd3cb2bf32287d79fbf3863e079af77bd
Kremor Pärt <kremor.part@gmail.com>
2018-01-22 15:58:21 -0600

    Functions changed to reflect the design in the readme.

--- __main__.py
+++ __main__.py
@@ -5,241 +5,125 @@ import sys
 
 from discord.ext import commands
 
+from taboo import Taboo
+
 bot = commands.Bot('\\')
 
-taboo_data = {}
+games = {}
 
 
-@bot.command(pass_context=True)
-async def hi(context: commands.Context, *args):
-    """Says hi to the user"""
-    if args:
-        if args[0] == 'rose':
-            rose = None
-            for member in context.message.server.members:
-                if member.id == '347681561946357761':
-                    rose = member
-            await bot.say('hi ' + rose.mention + ' you are :lit: AF')
-    else:
-        await bot.say('hi ' + context.message.author.mention)
+""" ---... Cards management ...--- """
 
 
-@bot.command(pass_context=True)
-async def count(context: commands.Context, seconds: str):
-    """counts down"""
-    bot.loop.create_task(count_down(int(seconds), context.message.channel))
+@bot.command(name='addcard', aliases=['ac'], pass_context=True)
+async def add_card(context):
+    pass
 
 
-async def count_down(seconds: int, channel):
-    await bot.wait_until_ready()
-    await asyncio.sleep(seconds)
-    await bot.send_message(channel, 'Tines out!')
+@bot.command(name='removecard', aliases=['rc'], pass_context=True)
+async def remove_card(context):
+    pass
 
 
-@bot.event
-async def on_message(message):
-    if message.content == 'matrix':
-        await bot.send_message(message.channel, 'winner')
-    else:
-        await bot.process_commands(message)
+""" ---... Taboo Game ...--- """
 
 
-# =====.===== TABOO =====.=====
+@bot.command(name='buzz', pass_context=True)
+async def buzz(context):
+    pass
 
 
-@bot.group(aliases=['t'], pass_context=True)
-async def taboo(ctx):
-    """Taboo game."""
-    if ctx.invoked_subcommand is None:
-        await bot.say('No arguments')
-
-
-@taboo.command(name='new', aliases=['n'], pass_context=True)
-async def taboo_new(context: commands.Context):
-    """Creates a new taboo game"""
+@bot.command(name='join', aliases=['j'], pass_context=True)
+async def join(context):
     server_id = context.message.server.id
-
-    connection = sqlite3.connect('db/server-settings.db')
-    cursor = connection.cursor()
-
-    cursor.execute("create table if not exists taboo (server text, rounds int, seconds int)")
-
-    settings = {}
-
-    for row in cursor.execute("select server, rounds, seconds from taboo"):
-        server, rounds, seconds = row
-        if server == server_id:
-            settings = {'server': server, 'rounds': rounds, 'seconds': seconds, 'teams': []}
-            break
-
-    if not settings:
-        cursor.execute("insert into taboo ('{}', {}, {})".format(server_id, 10, 120))
-        settings = {'server': server_id, 'rounds': 10, 'seconds': 120, 'teams': []}
-
-    cursor.close()
-    connection.commit()
-    connection.close()
-
-    taboo_data[server_id] = settings
-
-    await bot.say('''
-    New game created.
-    
-    \tTo add a new team - \\taboo addteam @member1 @member 2
-    \tTo start the game - \\taboo start
-    ''')
-
-
-@taboo.command(name='addteam', aliases=['at'], pass_context=True)
-async def taboo_add_team(context, member1, member2):
-    """
-    Adds a new team to the current game.
-    """
-    if not member1 or not member2:
-        await  bot.say('Not enough arguments.')
-
-    member1 = member1[2:-1] if '!' not in member1 else member1[3:-1]
-    member2 = member2[2:-1] if '!' not in member2 else member2[3:-1]
-
-    p1 = None
-    p2 = None
-
-    server = context.message.server
-    for member in server.members:
-        if member.id == member1:
-            p1 = member
-        if member.id == member2:
-            p2 = member
-        if p1 and p2:
-            break
-
-    if not p1 or not p2:
-        await bot.say('You need to tag a person dummy.')
-    elif p1 == p2:
-        await bot.say(p1.mention + ' you need another person to play with you, dummy.')
-    elif p1.bot:
-        await bot.say(p1.mention + ' can\'t play, you dummy.')
-    elif p2.bot:
-        await bot.say(p1.mention + ' can\'t play, you dummy.')
-    else:
-        if server.id not in taboo_data:
-            server_id = server.id
-
-            connection = sqlite3.connect('db/server-settings.db')
-            cursor = connection.cursor()
-
-            cursor.execute(
-                "create table if not exists taboo (server text, rounds int, seconds int)")
-
-            settings = {}
-
-            for row in cursor.execute("select server, rounds, seconds from taboo"):
-                serv, rounds, seconds = row
-                if serv == server_id:
-                    settings = {'server': serv, 'rounds': rounds, 'seconds': seconds, 'teams': []}
-                    break
-
-            if not settings:
-                cursor.execute("insert into taboo values(?, ?, ?)", (server_id, 10, 120))
-                settings = {'server': server_id, 'rounds': 10, 'seconds': 120, 'teams': []}
-
-            cursor.close()
-            connection.commit()
-            connection.close()
-
-            taboo_data[server_id] = settings
-
-        teams = taboo_data[server.id]['teams']
-
-        found = False
-
-        for team in teams:
-            if p1 == team[0]:
-                await bot.say('{} is already in a team with {}.'.format(p1.mention,
-                                                                        team[1].mention))
-                found = True
-            elif p1 == team[1]:
-                await bot.say('{} is already in a team with {}.'.format(p1.metion, team[0].mention))
-                found = True
-
-            if p2 == team[0]:
-                await bot.say('{} is already in a team with {}.'.format(p2.mention,
-                                                                        team[1].mention))
-                found = True
-            elif p2 == team[1]:
-                await bot.say('{} is already in a team with {}.'.format(p2.metion, team[0].mention))
-                found = True
-
-        if not found:
-            teams.append((p1, p2))
-            await bot.say('{} and {} where added to the game.'.format(p1.mention, p2.mention))
-
-
-@taboo.command(name='leaderboard', aliases=['l'], pass_context=True)
-async def taboo_leaderboard(context: commands.Context):
-    """
-    Shows the server's leaderboard
-    """
-    server = context.message.server
-    if server:
-        await bot.say("{}'s leaderboard.".format(server.name))
-
-
-@taboo.command(name='remteam', aliases=['rt'], pass_context=True)
-async def taboo_remove_team(context):
-    """
-    Removes you from the game. It will also remove your partner.
-    """
-    server_id = context.message.server.id
-
-    if server_id not in taboo_data:
-        await bot.say('There\'s not game currently')
-        return
-
     author = context.message.author
-
-    teams = taboo_data[server_id]['teams']
-    for i, team in enumerate(teams):
-        if author in team:
-            await bot.say('{} and {} where removed from the game.'.format(team[0].mention,
-                                                                          team[1].mention))
-        teams.pop(i)
-
-
-async def taboo_loop(server_id: str, channel_id: str, seconds: int, rounds: int):
-    playing = True
-    round_counter = 0
-    while playing and round_counter < rounds:
-        await asyncio.sleep(seconds)
-
-
-@taboo.command(name='start', aliases=['s'])
-async def taboo_start():
-    """Starts the game"""
-    bot.loop.create_task(taboo_loop())
-
-
-@taboo.command(name='stop', pass_context=True)
-async def taboo_stop(context):
-    """Stops the current game"""
-    taboo_data[context.message.server.id]['playing'] = False
-
-
-@taboo.command(name='time', aliases=['t'])
-async def taboo_time(seconds):
-    """
-    Sets the maximum length per turn in seconds (default is 120)
-    """
     pass
 
 
-@taboo.command(name='wpg')
-async def taboo_fist_of(wins_per_game: int):
-    """
-    Sets the number of maximum wins per game (default is 5).
-    The game will end when a team reaches that many wins.
-    """
+@bot.command(name='leave', aliases=['l'], pass_context=True)
+async def leave(context):
     pass
+
+
+@bot.command(name='skip', aliases=['s'], pass_context=True)
+async def skip(context):
+    pass
+
+
+@bot.command(name='start', pass_context=True)
+async def start(context):
+    pass
+
+
+@bot.command(name='stop', pass_context=True)
+async def stop(context):
+    pass
+
+
+@bot.command(name='taboo', aliases=['t'], pass_context=True)
+async def taboo(context):
+    server_id = context.message.server.id
+
+    taboo_game = None if server_id not in games else games[server_id]
+
+    if not taboo_game:
+        taboo_game = Taboo()
+        await bot.say("""
+        Game created. The game will start in 5 minutes.
+            Type `\\join` to join the game.
+        """)
+    elif taboo_game.playing:
+        await bot.say("Can't start a new game when there's one taking place.")
+    else:
+        await bot.say("A game was already created and will start soon.")
+
+    games[server_id] = taboo_game
+
+
+""" ---... Miscellaneous ...--- """
+
+
+@bot.command(name='addadmin', aliases=['aa'], pass_context=True)
+async def add_admin(context):
+    pass
+
+
+@bot.command(name='listadmins', aliases=['la'], pass_context=True)
+async def list_admins(context):
+    pass
+
+
+@bot.command(name='listcon', aliases=['lc'], pass_context=True)
+async def list_conf(context):
+    pass
+
+
+@bot.command(name='removeadmin', aliases=['ra'], pass_context=True)
+async def remove_admin(context):
+    pass
+
+
+@bot.command(name='repo')
+async def repo():
+    await bot.say('https://github.com/Kremor/gally')
+
+
+@bot.command(name='setchannel', aliases=['sc'], pass_context=True)
+async def set_channel(context):
+    pass
+
+
+@bot.command(name='setrounds', aliases=['sr'], pass_context=True)
+async def set_rounds(context, *args):
+    pass
+
+
+@bot.command(name='settimer', aliases=['st'], pass_context=True)
+async def set_timer(context, *args):
+    pass
+
+
+""" ---... MAIN ...--- """
 
 
 def main():
